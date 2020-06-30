616c85b27a28f11639d4c10fe5d2fb41d9811d44
Anas Alfaihan <narotoaozomake99@gmail.com>
2020-06-30 14:55:52 -0700

    .

--- venv/Scripts/easy_install-3.7-script.py
+++ venv/Scripts/easy_install-3.7-script.py
@@ -1,4 +1,4 @@
-#!"C:\Users\Anas Alfaihan\PycharmProjects\flask_demo\venv\Scripts\python.exe"
+#!"C:\Users\Anas Alfaihan\PycharmProjects\untitled2\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.7'
 __requires__ = 'setuptools==40.8.0'
 import re
